da872528c15f68914b18d59563ba2c75927678c9
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-12-01 12:18:33 -0500

    Agregar archivos de proyecto.

---
+++ ReservaCanchas-Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReservaCanchas_Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ReservaCanchas-Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ReservaCanchas_Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
