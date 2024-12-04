453f538f2d8262b2d6954a8e642dbe01a191d0c2
josue riera <josueriera23@hotmail.com>
2024-12-04 00:20:33 -0500

    Chris Valenciaaaa

---
+++ ReservaCanchas-Maui/AdminViews/GestionarUsers.xaml.cs
@@ -0,0 +1,10 @@
+namespace ReservaCanchas_Maui.AdminViews;
+
+public partial class GestionarUsers : ContentPage
+{
+	public GestionarUsers()
+	{
+		InitializeComponent();
+		Console.WriteLine("Prueba");
+	}
+}
