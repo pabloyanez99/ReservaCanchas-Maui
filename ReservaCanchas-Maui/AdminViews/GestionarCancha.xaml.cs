a66d6a6e8cfb071cee11303e7b63418d074ded0e
Pablo Yánez Alvear <75266773+pabloyanez99@users.noreply.github.com>
2024-12-06 13:14:52 -0500

    Update GestionarCancha.xaml.cs

--- ReservaCanchas-Maui/AdminViews/GestionarCancha.xaml.cs
+++ ReservaCanchas-Maui/AdminViews/GestionarCancha.xaml.cs
@@ -1,9 +1,27 @@
+using ReservaCanchas_Maui.Models;
+using ReservaCanchas_Maui.Repositories;
+
 namespace ReservaCanchas_Maui.AdminViews;
 
 public partial class GestionarCancha : ContentPage
 {
-	public GestionarCancha()
+    public Usuario _usuario;
+    public Complejo _complejo;
+    public Cancha _cancha;
+    public ReservaRepositroy _repository;
+    public Reserva _reserva;
+    public GestionarCancha()
 	{
-		InitializeComponent();
+		//InitializeComponent();
 	}
+
+    private void OnGuardarCambiosClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnRegresarClicked(object sender, EventArgs e)
+    {
+
+    }
 }
